5bd9b4750f01ad331a796f4cb209180842697f06
didillysquat <didillysquat@gmail.com>
2019-04-04 23:31:56 +0300

    added --assess_directinality

--- dbg.py
+++ dbg.py
@@ -8,7 +8,7 @@ import sys
 class PyDBGAssembler:
     def __init__(
             self, fa_or_fq_file_path_list, kmer_len, is_relaxed, is_pick_random, verbose, remove,
-            low_abund_filter_threshold, output_path=None,
+            low_abund_filter_threshold, assess_directionality, output_path=None,
             ):
         self.cwd = os.path.dirname(os.path.realpath(__file__))
         if output_path is not None:
@@ -41,7 +41,7 @@ class PyDBGAssembler:
         else:
             self.kmer_list_for_contigs = list(self.kmer_count_dict.keys())
         self.num_kmers = len(self.kmer_list_for_contigs)
-        # A set that holds all of the kmers that have already been used to make a contig
+        # A set that holds all of the kmers that have already been used to seed a contig extension
         self.used_kmers_set = set()
         # A list that holds all of the contigs assembled so far
         self.list_of_completed_contigs = []
@@ -56,6 +56,14 @@ class PyDBGAssembler:
         self.current_build_kmer_rev = None
         self.verbose = verbose
         self.remove = remove
+        self.assess_directionality = assess_directionality
+        # Attributes used in directionality assessment
+        self.lhs_seed_kmer = None
+        self.rhs_seed_kmer = None
+        self.forward_contig_list = None
+        self.reverse_contig_list = None
+        # The set of kmers that have been used in building the current contig
+        self.temp_used_build_kmers_set = set()
 
 
 
@@ -72,10 +80,10 @@ class PyDBGAssembler:
             if kmer_key not in self.used_kmers_set:
                 self.current_build_kmer_fwd = kmer_key
                 self.current_build_kmer_rev = self._rev_comp(self.current_build_kmer_fwd)
-                contig_as_string, c = self._get_contig()
-                for y in c:
-                    self.used_kmers_set.add(y)
-                    self.used_kmers_set.add(self._rev_comp(y))
+                contig_as_string, contig_kmer_list = self._get_contig()
+                for used_kmer in contig_kmer_list:
+                    self.used_kmers_set.add(used_kmer)
+                    self.used_kmers_set.add(self._rev_comp(used_kmer))
                 self.list_of_completed_contigs.append(contig_as_string)
         print("\nDone.")
 
@@ -122,6 +130,19 @@ class PyDBGAssembler:
             self.completed_contigs_as_fasta.extend([f'>{i}', f'{contig_seq}'])
 
     def _get_contig(self):
+        """This is where we go from a seed kmer and return a contig as both a string and as a list of kmers.
+        If we are not assessing directionality (--assess_directionality) then this takes the form of extending
+        the kmer forwards and then back and then returning it. If we are assessing directionality then we extend
+        the kmer forward and back as before but then we take the first and last kmer of the assembled contig
+        and do a new assembly in the forward and rever direction using these two kmers as seeds for each respectively.
+        We then check to see if the two contigs created agree. If they do, great, if the don't then we return
+        (i.e. choose) the contig that has the highest mean support of its kmers. If we are working with the --remmove
+        flag, this means taht we are strictly only allowing each kmer to be use once across the entire assembly
+        of all contigs. Rather than directly delete the used kmers when in remove mode we put them into a used
+        list and see if kmers are in the used list then at the end of get contig creation we remove the kmers
+        from the dict. This way we can easily reinstate the set of kmers available when doing directionality
+        assessment"""
+
         self.kmer_list_of_contig_fwd = self._get_contig_forward(self.current_build_kmer_fwd)
 
         self.kmer_list_of_contig_rev = self._get_contig_forward(self.current_build_kmer_rev)
@@ -132,7 +153,85 @@ class PyDBGAssembler:
             self.kmer_list_of_contig = self.kmer_list_of_contig_fwd
         else:
             self.kmer_list_of_contig = [self._rev_comp(x) for x in self.kmer_list_of_contig_rev[-1:0:-1]] + self.kmer_list_of_contig_fwd
+            if self.assess_directionality and self.kmer_list_for_contigs:
+                return self._assess_directionality()
+        if self.remove:  # delete the kmers in the temp used set from the dict before exiting
+            self._del_temp_kmers_from_count_dict()
         return self._contig_to_string(), self.kmer_list_of_contig
+
+    def _assess_directionality(self):
+        self._make_forward_extension_contig()
+        self._make_reverse_extension_contig()
+        if not self.reverse_contig_list or not self.forward_contig_list:
+            if self.reverse_contig_list:
+                self._del_temp_kmers_from_count_dict(self.reverse_contig_list)
+                return self._contig_to_string(self.reverse_contig_list), self.reverse_contig_list
+            elif self.forward_contig_list:
+                self._del_temp_kmers_from_count_dict(self.forward_contig_list)
+                return self._contig_to_string(self.forward_contig_list), self.forward_contig_list
+            else:  # neither list is good
+                pass
+
+
+        return self._if_agree_return_fwd_contig_else_return_contig_with_most_support()
+
+    def _if_agree_return_fwd_contig_else_return_contig_with_most_support(self):
+        if self.forward_contig_list == self.reverse_contig_list:  # If no disagreement then contig is good
+            if self.remove:
+                self._del_temp_kmers_from_count_dict()
+            return self._contig_to_string(self.forward_contig_list), self.forward_contig_list
+        else:  # contigs disagree then take the contig that has the highest average support for its kmers
+            return self._return_contig_with_highest_average_kmer_support()
+
+    def _make_reverse_extension_contig(self):
+        self.rhs_seed_kmer = self._rev_comp(self.kmer_list_of_contig[-1])
+        # reset the used kmer set
+        self._reset_temp_used_kmer_set()
+        self.reverse_contig_list = [self._rev_comp(x) for x in self._get_contig_forward(self.rhs_seed_kmer)[-1:0:-1]]
+
+    def _make_forward_extension_contig(self):
+
+        self.lhs_seed_kmer = self.kmer_list_of_contig[0]
+        # reset the used kmer set
+        self._reset_temp_used_kmer_set()
+        self.forward_contig_list = self._get_contig_forward(self.lhs_seed_kmer)
+
+    def _return_contig_with_highest_average_kmer_support(self):
+        forward_mean_kmer_support = self._calc_mean_kmer_support(self.forward_contig_list)
+        reverse_mean_kmer_support = self._calc_mean_kmer_support(self.reverse_contig_list)
+        if forward_mean_kmer_support > reverse_mean_kmer_support:
+            if self.remove:
+                self._del_temp_kmers_from_count_dict(self.forward_contig_list)
+            return self._contig_to_string(self.forward_contig_list), self.forward_contig_list
+        else:
+            if self.remove:
+                self._del_temp_kmers_from_count_dict(self.reverse_contig_list)
+            return self._contig_to_string(self.reverse_contig_list), self.reverse_contig_list
+
+    def _reset_temp_used_kmer_set(self):
+        self.temp_used_build_kmers_set = set()
+
+    def _calc_mean_kmer_support(self, kmer_list):
+        return sum([self.kmer_count_dict[kmer] for kmer in kmer_list])/len(kmer_list)
+
+    def _del_temp_kmers_from_count_dict(self, kmer_set_of_list=None):
+        """ When we are working with --remove flag then we are strictly only allowing each kmer to be used once
+        in building the kmers. We keep track of which kmers have been used through the contig creations using
+        the self.temp_used_build_kmers_set. At then end of the contig building we need to make sure that these kmers
+        are deleted from the dict and the kmer set is reset."""
+
+        if kmer_set_of_list is None:
+            for kmer in self.temp_used_build_kmers_set:
+                del self.kmer_count_dict[kmer]
+                if self._rev_comp(kmer) != kmer: # cannot delete a seq if rev comp is same as fwd comp.
+                    del self.kmer_count_dict[self._rev_comp(kmer)]
+        else:
+            for kmer in kmer_set_of_list:
+                del self.kmer_count_dict[kmer]
+                if self._rev_comp(kmer) != kmer: # cannot delete a seq if rev comp is same as fwd comp.
+                    del self.kmer_count_dict[self._rev_comp(kmer)]
+        self.temp_used_build_kmers_set = set()
+
 
     def _contig_is_circular(self, kmer):
         return self.current_build_kmer_fwd in self._fwd_seq_generator(kmer)
@@ -143,7 +242,9 @@ class PyDBGAssembler:
         while True:
             # cand is abbrev. for candidate
             cand_kmers = [(cand, self.kmer_count_dict[cand]) for cand in
-                          self._fwd_seq_generator(c_fw[-1]) if cand in self.kmer_count_dict]
+                          self._fwd_seq_generator(c_fw[-1]) if
+                          cand in self.kmer_count_dict if
+                          cand not in self.temp_used_build_kmers_set]
 
             if not self.is_relaxed: # operating in strict mode
                 # If more than one of the candidate kmers exists in the dictionary
@@ -178,7 +279,8 @@ class PyDBGAssembler:
 
             c_fw.append(candidate)
             if self.remove:
-                del self.kmer_count_dict[candidate]
+                self.temp_used_build_kmers_set.add(candidate)
+                self.temp_used_build_kmers_set.add(self._rev_comp(candidate))
 
         return c_fw
 
@@ -259,8 +361,11 @@ class PyDBGAssembler:
             yield x + km[:-1]
 
 
-    def _contig_to_string(self):
-        return self.kmer_list_of_contig[0] + ''.join(kmer[-1] for kmer in self.kmer_list_of_contig[1:])
+    def _contig_to_string(self, contig_list=None):
+        if contig_list is None:
+            return self.kmer_list_of_contig[0] + ''.join(kmer[-1] for kmer in self.kmer_list_of_contig[1:])
+        else:
+            return contig_list[0] + ''.join(kmer[-1] for kmer in contig_list[1:])
 
 
 def process_args():
@@ -296,6 +401,14 @@ def process_args():
     parser.add_argument("--remove",
                         help="When set, this flag will mean that every kmer in the dictionary may stricly only be used"
                              "once.", action="store_true", default=False)
+    parser.add_argument("--assess_directionality",
+                        help="If set to true, after a contig is generated from a seed kmer, rather than directly "
+                             "returning that contig, the first and last kmer of this contig are used as seed kmers"
+                             " to make two additional contigs, one representing extension in the forward direction "
+                             "and one representing extension in the reverse direction. These two contigs are assessed "
+                             "for similarity. If they are identical, one of the contigs will be returned. If there are"
+                             " differences between the contigs, the contig with the highest average per kmer support"
+                             " will be returned.", action="store_true", default=False)
     return parser.parse_args()
 
 
@@ -304,5 +417,6 @@ if __name__ == "__main__":
     dbga = PyDBGAssembler(
         fa_or_fq_file_path_list=args.files, kmer_len=args.kmer_length,
         output_path=args.output_path, is_relaxed=args.relaxed, is_pick_random=args.pick_by_random,
-        verbose=args.verbosity, remove=args.remove, low_abund_filter_threshold=args.abund_threshold)
+        verbose=args.verbosity, remove=args.remove, low_abund_filter_threshold=args.abund_threshold,
+        assess_directionality=args.assess_directionality)
     dbga.do_assembly()
